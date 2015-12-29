98413d7a983c4108cf1e906bfe05bb6820d84882
RinatK <rkashapov85@gmail.com>
2015-12-29 17:35:58 +0300

    working on product/show.php

---
+++ product/show.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Магазин_ main</title>
+    <link href="../_/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../_/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../_/css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+    <header>dfdfdfdf</header>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <a href="#">< Назад к результатам поиска</a>
+            </div>
+
+            <div class="col-xs-12">
+                <h3>ПОДУШКА CLASSIC BY TOGASS "АНТИСТРЕСС", 70x70 см</h3>
+            </div>
+            <div class="col-xs-12">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
+                <img src="../images/podushka.jpg" style="width:554px; height:416px;">
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
+
+                    <p>TOGAS <span class="country_product">(Россия)</span></p>
+                    <hr class="">
+                    <div class="description_product">
+                        <p>Подушка с чехлом из уникальной ткани «Антистресс» поможет расслабиться после трудного рабочего дня, а на утро почувствовать силы для новых достижений.</p>
+                        <div class="properties_product">
+                            <p>Размер 70x70 см</p>
+                            <p>Артикул 123456</p>
+                            <a href="#">Вся информация о товаре <i class="fa fa-info-circle"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <h3>Похожие объявления</h3>
+                <hr class="blue_line">
+            </div>
+        </div>
+    </div>
+
+<?php include "../_/components/php/footer.php"?>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="../_/js/bootstrap.min.js"></script>
+</body>
+</html>
